3f01824d1863db5e94bc0e130f1c6a7bf3830b96
404DontSleep <404dontsleep@gmail.com>
2025-05-20 15:40:24 +0700

    feat: update portfolio page

--- src/layouts/tailwind/MainLayout.tsx
+++ src/layouts/tailwind/MainLayout.tsx
@@ -4,7 +4,7 @@ import Navbar from "../../components/tailwind/Navbar";
 export default function MainLayout() {
   return (
     <section className='flex flex-col h-screen p-4 gap-4'>
-      <Navbar />
+      <Navbar config={{ links: [], title: "" }} />
       <main className='flex-1 overflow-y-auto flex flex-col'>
         <Outlet />
       </main>
